8ac8954a04b349a19bb58224a1ea700e93c9a77b
David Tung <dtung.725@gmail.com>
2021-07-15 22:03:36 -0700

    Removed dummy code

--- src/main.cc
+++ src/main.cc
@@ -7,13 +7,6 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	Layer l1(10, "sigmoid");
-
-	vector<double> values = l1.get_values();
-	for (double v: values) {
-		cout << v << endl;
-	}
-
 	NeuralNetwork nn;
 	cout << "Created a neural network?" << endl;
 }
